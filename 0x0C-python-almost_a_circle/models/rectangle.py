9fa51130d0c5e3c4aa864ea1962d1f533b616795
Hendrix Ebrudu <hendrixobuks@gmail.com>
2023-09-19 23:21:46 +0100

    Almost-done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,7 +73,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Sets the value for x"""
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("x must be an integer")
 
         if value < 0:
@@ -84,10 +84,85 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Sets the value for y"""
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("y must be an integer")
 
         if value < 0:
             raise ValueError("y must be >= 0")
 
         self.__y = value
+
+    def area(self):
+        """Defines the rectangle using # """
+        return self.__height * self.__width
+
+    def display(self):
+        """Prints the rectangle using #"""
+        for y in range(self.y):
+            print("")
+
+        for row in range(self.__height):
+            for x in range(self.x):
+                print(" ", end="")
+            for column in range(self.__width):
+                print("#", end="")
+            print("")
+
+    def __str__(self):
+        """Return the dictionary representations of a Rectangle"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.__width, self.__height)
+
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute
+        Update the Rectangle.
+                Args:
+                    *args (ints): New attribute values.
+                        - 1st argument represents id attribute
+                        - 2nd argument represents width attribute
+                        - 3rd argument represent height attribute
+                        - 4th argument represents x attribute
+                        - 5th argument represents y attribute
+                    **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of a Rectangle"""
+
+        obj_dictionary = {'id': self.id, 'width': self.__width,
+                          'height': self.__height, 'x': self.__x,
+                          'y': self.__y}
+
+        return obj_dictionary
