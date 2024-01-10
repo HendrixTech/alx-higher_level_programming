5b469201012bb741f956a2d173c4e6b362a15c76
HendrixTech <hendrixobuks@gmail.com>
2024-01-09 19:31:20 -0800

    tasks 6 to 8

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if (!parseInt(x)) {
+	console.log('Missing number of occurennces');
+} else {
+	for (let i = 0; i < parseInt(x); i++) {
+		console.log('C is fun');
+	}
+}
---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const x = process.argv[2];
+
+if (!parseInt(x)) {
+	console.log('Missing size');
+} else {
+	for (let i = 0; i < x; i++) {
+		let row = '';
+		for (let j = 0; j < x; j++){
+			row = row + 'X';
+		}
+		console.log(row);
+	}
+}
