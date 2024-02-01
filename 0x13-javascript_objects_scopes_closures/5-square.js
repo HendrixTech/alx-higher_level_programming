572487c70bc4ea6c4f1efbcedfaa0aa658917802
Hendrix Ebrudu <hendrixobuks@gmail.com>
2024-02-01 23:49:19 +0100

    tasks 5 & 6

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/**
+ * Define square class that inherits from Rectangle class
+ */
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+    constructor(size) {
+        super(size, size);
+    }
+}
+module.exports = Square;
