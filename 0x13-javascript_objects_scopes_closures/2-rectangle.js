e785fbb0b900e128ed194c7454e2262ff63eb4db
HendrixTech <hendrixobuks@gmail.com>
2024-02-02 15:08:00 -0800

    semistandard

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,17 +3,17 @@
  * Validate parameters
  */
 class Rectangle {
-    constructor(w, h) {
-        if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
-            this.width = w;
-            this.height = h;
-        }
-        // Instead of printing undefined, you can set a default value or message here
-        // else {
-        //     this.width = w;
-        //     this.height = h;
-        //     console.log("one or more arguments undefined")
-        // }
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+    // Instead of printing undefined, you can set a default value or message here
+    // else {
+    //     this.width = w;
+    //     this.height = h;
+    //     console.log("one or more arguments undefined")
+    // }
+  }
 }
 module.exports = Rectangle;
--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -3,24 +3,24 @@
  * Defines a class and creates an instance
  */
 class Rectangle {
-    constructor(w, h) {
-        if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0){
-            this.width = w;
-            this.height = h;
-        }
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 
-    print() {
-        for (let i = 0; i < this.height; i++) {
-            let myVar = '';
-            let y = 0;
-            while (y < this.width) {
-                myVar += 'X';
-                y++;
-            }
-        
-        console.log(myVar);
-        }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let myVar = '';
+      let y = 0;
+      while (y < this.width) {
+        myVar += 'X';
+        y++;
+      }
+
+      console.log(myVar);
     }
+  }
 }
 module.exports = Rectangle;
